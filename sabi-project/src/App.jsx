5be7666a0801dcb9d873e942109fea5a6b1fe9d2
sabi714 <117071260+sabi714@users.noreply.github.com>
2022-11-22 14:56:00 +0400

    Add files via upload

---
+++ sabi-project/src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import"./index.css";
+
+const App = () =>{
+  return (
+    <div class="sabi">
+      <h3>Nehmetova Sabina</h3>
+      <p> Now I can create component</p>
+    </div>
+  )
+}
+export default App
---
+++ sabi-project/src/main.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import Header from "./components/header/header";
+
+ReactDOM.createRoot(document.getElementById("root")).render(<App />);
